678c30ee59693e74bff1417246260c483cafffb9
Tabachenkov <125300486+Tabachenkov@users.noreply.github.com>
2023-10-30 11:16:26 +0400

    Update random.c

--- random.c
+++ random.c
@@ -4,33 +4,39 @@
 #include <unistd.h>
 #include <stdlib.h>
 #define CONV(ops) ((RandomSourceOperations *) (ops))
-double random_next(RandomSource *src) {
-	const char *source = "/dev/urandom";
-	unsigned long long num;
-	int fd = open(source, O_RDONLY, 0);
-	read(fd, &num, sizeof(num));
-	close(fd);
-	double new = num ? (1.0 * (num - 1)) / (1.0 * ULLONG_MAX) : 0.0;
-	return (src->x = new);
+double
+random_next(RandomSource *src)
+{
+    const char *source = "/dev/urandom";
+    unsigned long long num;
+    int fd = open(source, O_RDONLY, 0);
+    read(fd, &num, sizeof(num));
+    close(fd);
+    double new = num ? (1.0 * (num - 1)) / (1.0 * ULLONG_MAX) : 0.0;
+    return (src->x = new);
 }
-RandomSource *random_destroy(RandomSource *src) {
-	free(src->ops);
-	free(src);
-	return NULL;
+RandomSource *
+random_destroy(RandomSource *src)
+{
+    free(src->ops);
+    free(src);
+    return NULL;
 }
-RandomSource *random_random_factory(void) {
-	RandomSource *rr = calloc(1, sizeof(*rr));
-	if (rr == NULL) {
-		return NULL;
-	}
-	rr->x = 0.0;
-	rr->linear_x = 0;
-	rr->ops = calloc(1, sizeof(*(CONV(rr->ops))));
-	if (rr->ops == NULL) {
-		free(rr);
-		return NULL;
-	}
-	CONV(rr->ops)->next = random_next;
-	CONV(rr->ops)->destroy = random_destroy;
-	return rr;
+RandomSource *
+random_random_factory(void)
+{
+    RandomSource *rr = calloc(1, sizeof(*rr));
+    if (rr == NULL) {
+        return NULL;
+    }
+    rr->x = 0.0;
+    rr->linear_x = 0;
+    rr->ops = calloc(1, sizeof(*(CONV(rr->ops))));
+    if (rr->ops == NULL) {
+        free(rr);
+        return NULL;
+    }
+    CONV(rr->ops)->next = random_next;
+    CONV(rr->ops)->destroy = random_destroy;
+    return rr;
 }
